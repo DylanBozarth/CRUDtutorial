3ffcdd0eb0872c7cacdfbe1b9895dea3654a9727
dylan <dylanabozarth@gmail.com>
2020-11-11 20:51:55 -0800

    now with backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      AAAAAAA
+      <h1>CRUD</h1>
+      <input type="text" name="moviename" placeholder="Movie name"></input>{" "}
+      
+      <input type="text" name="moviereview" placeholder="movie review"></input>
+      <br />
+      <button>Submit</button>
     </div>
   );
 }
