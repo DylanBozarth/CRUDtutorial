298a9b7dd354eae11d84ff2f97ebbfa17909dd6a
dylan <dylanabozarth@gmail.com>
2020-11-12 09:48:29 -0800

    node documentation

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
-const express = require("express");
-const app = express();
-// set code to a url 
-// req = request  res = response
-app.get("/", (req, res) => {
-    res.send('AAAAa')
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
 });
-// see the package.json for the commands
-app.listen(3003, () => {
-    console.log('the backend words 2')
-}
-)
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
